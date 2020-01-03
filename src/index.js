bd8463d3ee8bbfa483867b9de7f39e2bf9d0b831
John E Shepler Jr <john.sheplerjr@gmail.com>
2020-01-03 16:12:23 -0700

    first

---
+++ src/index.js
@@ -0,0 +1,34 @@
+require('./models/Users')
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/authRoutes');
+const requireAuth = require('./middlewares/requireAuth');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(authRoutes);
+
+const mongoUri = 'mongodb+srv://shep222:Amazing2020$$@cluster0-lymxe.mongodb.net/test?retryWrites=true&w=majority'
+
+mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+})
+
+mongoose.connection.on('connected', ()=>{
+    console.log('Connected to Mongo');
+})
+
+mongoose.connection.on('error', (err)=>{
+    console.error('Error connecting to Mongo', err);
+})
+
+app.get('/', requireAuth, (req,res)=>{
+    res.send(`Your email: ${req.user.email}`)
+});
+
+app.listen(3000, ()=>{
+    console.log('listening on port 3000');
+})
---
+++ src/routes/authRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const User = mongoose.model('User');
+
+const router = express.Router();
+
+router.post('/signup', async (req, res)=>{
+    try {
+        const {email, password} = req.body;
+        const user = new User({email, password});
+        await user.save();
+        const token = jwt.sign( {userId: user._id}, 'MY_SECRET_KEY')
+        res.send({token: token})
+    } catch (err) {
+        return res.status(422).send(err.message);
+    }
+})
+
+module.exports = router;
