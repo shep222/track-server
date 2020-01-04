cadc87a12bb5fc86017c33aa2a7cea4e216df1fa
John E Shepler Jr <john.sheplerjr@gmail.com>
2020-01-03 18:06:34 -0700

    brcypt

--- src/middlewares/requireAuth.js
+++ src/middlewares/requireAuth.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
 
-module.export = (req, res, next) => {
+module.exports = (req, res, next) => {
     const { authorization } = req.headers;
     if(!authorization) {
         return res.status(401).send({error: 'You must be signed in'})
